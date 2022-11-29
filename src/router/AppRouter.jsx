9e3dca589aab9903fbe3f4f3ad1aa204e1816b24
cris-gs <cristophergs2001@gmail.com>
2022-11-28 21:52:31 -0600

    first commit

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { LoginPages } from '../auth/pages/LoginPages'
-import { DcPage } from '../heroes/pages/DcPage'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { Route, Routes } from 'react-router-dom';
+
+import { LoginPages } from '../auth';
+import { HeroesRoutes } from '../heroes';
+
+import { Navbar } from '../ui';
+
+
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-          
-            <Route path="marvel" element={<MarvelPage />} />
-            <Route path="dc" element={<DcPage />} />
 
             <Route path="login" element={<LoginPages />} />
             
-            <Route path="/" element={<Navigate to={"/marvel"} />} />
+            <Route path="/*" element={<HeroesRoutes />} />
+
         </Routes>
     </>
   )
